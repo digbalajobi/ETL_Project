cd1af2ba0caf0bd49ef131fa3682e421c685e2dc
Tyler Wiseman <58832417+wisemantyr@users.noreply.github.com>
2020-04-27 15:40:43 -0400

    Update etl_schema.sql

--- etl_schema.sql
+++ etl_schema.sql
@@ -20,9 +20,19 @@ CREATE TABLE january_flights(
 	lastseen_date VARCHAR,
 	lastseen_time VARCHAR
 	);
+  
+CREATE TABLE open_flights(
+	open_flights_id int primary key,
+	airline_name VARCHAR,
+	airline_alias VARCHAR,
+	iato_code VARCHAR,
+	icao_code VARCHAR,
+	airline_callsign VARCHAR,
+	country VARCHAR,
+	active VARCHAR);
 	
 SELECT * from iata_codes
 SELECT * from january_flights
-
+SELECT * from open_flights
 
 
